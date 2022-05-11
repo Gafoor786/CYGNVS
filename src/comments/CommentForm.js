6e1ced7324f58cbc8a7ad8d0dc4ab39d1dec3183
Gafoor786 <mohammedgafoor7@gmail.com>
2022-05-11 16:52:45 +0530

    Add Comments

--- src/comments/CommentForm.js
+++ src/comments/CommentForm.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 // import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
 import { makeStyles } from '@mui/styles';
@@ -35,19 +34,8 @@ const CommentForm = ({
       <TextField className={classes.field} id="outlined-multiline-flexible" label="Comments" multiline
         maxRows={2} variant="outlined" value={text} fullWidth required
         onChange={(e) => setText(e.target.value)} gutterBottom />
-        <Button variant="contained" className={classes.rightBtn} disabled={isTextareaDisabled} gutterBottom>Comment</Button>
-      
-      {/* <button className="comment-form-button" disabled={isTextareaDisabled}>
-        {submitLabel}
-      </button> */}
-      {hasCancelButton && (/* 
-        <button
-          type="button"
-          className="comment-form-button comment-form-cancel-button"
-          onClick={handleCancel}
-        >
-          Cancel
-        </button> */
+        <Button variant="contained" onClick={onSubmit} className={classes.rightBtn} disabled={isTextareaDisabled} gutterBottom>Comment</Button>
+      {hasCancelButton && (
         <Button variant="contained" disabled={isTextareaDisabled} onClick={handleCancel} gutterBottom>Cancel</Button>
       )}
     </form>
